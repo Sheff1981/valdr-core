89fb3ed57d8daa4f5865e7aabb95e974b96b9845
Sheff1981 <linaelizabethvoss@gmail.com>
2026-09-23 21:23:38 +0300

    v0.2 stage 6: test miner fee-rate block order

--- mining/fees_v02_test.go
+++ mining/fees_v02_test.go
@@ -68,3 +68,77 @@ func TestMinerClaimsSubsidyPlusFees(t *testing.T) {
 		t.Fatalf("coinbase claim=%d want=%d", got, config.InitialMiningReward+fee)
 	}
 }
+
+func TestMinerOrdersTransactionsByFeeRate(t *testing.T) {
+	chain := blockchain.New()
+	ownerKey, err := valdrcrypto.GenerateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	ownerAddress, err := valdrcrypto.AddressFromPublicKey(&ownerKey.PublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	recipientKey, err := valdrcrypto.GenerateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	recipientAddress, err := valdrcrypto.AddressFromPublicKey(&recipientKey.PublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	first, err := MineBlock(chain, ownerAddress, config.GenesisTimestamp+60, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	second, err := MineBlock(chain, ownerAddress, config.GenesisTimestamp+120, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	makeSpend := func(funding *transaction.Transaction, fee uint64, timestamp int64) *transaction.Transaction {
+		t.Helper()
+		tx := transaction.New(
+			[]transaction.Input{{
+				PreviousTransactionID: funding.TransactionID,
+				OutputIndex:           0,
+			}},
+			[]transaction.Output{{
+				Amount:    config.InitialMiningReward - fee,
+				Recipient: recipientAddress,
+			}},
+			timestamp,
+		)
+		if err := tx.Sign(ownerKey); err != nil {
+			t.Fatal(err)
+		}
+		return tx
+	}
+
+	low := makeSpend(first.Transactions[0], 100, config.GenesisTimestamp+180)
+	high := makeSpend(second.Transactions[0], 10_000, config.GenesisTimestamp+180)
+
+	mined, err := MineBlock(
+		chain,
+		ownerAddress,
+		config.GenesisTimestamp+180,
+		[]*transaction.Transaction{low, high},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(mined.Transactions) != 3 {
+		t.Fatalf("mined transaction count=%d want=3", len(mined.Transactions))
+	}
+	if mined.Transactions[1].TransactionID != high.TransactionID ||
+		mined.Transactions[2].TransactionID != low.TransactionID {
+		t.Fatalf(
+			"miner order=%s,%s want high=%s low=%s",
+			mined.Transactions[1].TransactionID,
+			mined.Transactions[2].TransactionID,
+			high.TransactionID,
+			low.TransactionID,
+		)
+	}
+}
