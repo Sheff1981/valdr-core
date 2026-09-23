8af8cdcfc272445c6fd00403a492afaa4c78a79b
Sheff1981 <linaelizabethvoss@gmail.com>
2026-09-23 21:25:05 +0300

    v0.2 stage 6: test miner block-size template cap

--- mining/fees_v02_test.go
+++ mining/fees_v02_test.go
@@ -4,7 +4,9 @@ import (
 	"testing"
 
 	"github.com/Sheff1981/valdr-core/config"
+	"github.com/Sheff1981/valdr-core/core/block"
 	"github.com/Sheff1981/valdr-core/core/blockchain"
+	"github.com/Sheff1981/valdr-core/core/consensus"
 	"github.com/Sheff1981/valdr-core/core/transaction"
 	valdrcrypto "github.com/Sheff1981/valdr-core/crypto"
 )
@@ -140,5 +142,109 @@ func TestMinerOrdersTransactionsByFeeRate(t *testing.T) {
 			high.TransactionID,
 			low.TransactionID,
 		)
+	}
+}
+
+func TestTemplateSelectionRespectsBlockByteLimit(t *testing.T) {
+	chain := blockchain.New()
+	ownerKey, err := valdrcrypto.GenerateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	ownerAddress, err := valdrcrypto.AddressFromPublicKey(&ownerKey.PublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	recipientKey, err := valdrcrypto.GenerateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	recipientAddress, err := valdrcrypto.AddressFromPublicKey(&recipientKey.PublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	first, err := MineBlock(chain, ownerAddress, config.GenesisTimestamp+60, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	second, err := MineBlock(chain, ownerAddress, config.GenesisTimestamp+120, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	makeSpend := func(funding *transaction.Transaction, fee uint64) *transaction.Transaction {
+		t.Helper()
+		tx := transaction.New(
+			[]transaction.Input{{
+				PreviousTransactionID: funding.TransactionID,
+				OutputIndex:           0,
+			}},
+			[]transaction.Output{{
+				Amount:    config.InitialMiningReward - fee,
+				Recipient: recipientAddress,
+			}},
+			config.GenesisTimestamp+180,
+		)
+		if err := tx.Sign(ownerKey); err != nil {
+			t.Fatal(err)
+		}
+		return tx
+	}
+
+	low := makeSpend(first.Transactions[0], 100)
+	high := makeSpend(second.Transactions[0], 10_000)
+	lowFee, err := chain.CalculateFees([]*transaction.Transaction{low})
+	if err != nil {
+		t.Fatal(err)
+	}
+	highFee, err := chain.CalculateFees([]*transaction.Transaction{high})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tip := chain.Tip()
+	height := tip.Height + 1
+	reward := consensus.BlockReward(height)
+	highCoinbase, err := transaction.NewCoinbase(
+		height,
+		ownerAddress,
+		reward+highFee,
+		config.GenesisTimestamp+180,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	oneTxProbe := block.New(
+		height,
+		tip.BlockHash,
+		config.GenesisTimestamp+180,
+		tip.Difficulty,
+		0,
+		[]*transaction.Transaction{highCoinbase, high},
+		"",
+	)
+
+	selected, fees, err := selectTransactions(
+		chain,
+		tip,
+		height,
+		ownerAddress,
+		config.GenesisTimestamp+180,
+		reward,
+		[]transactionCandidate{
+			{tx: low, fee: lowFee, size: low.SerializedSize()},
+			{tx: high, fee: highFee, size: high.SerializedSize()},
+		},
+		oneTxProbe.SerializedSize(),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(selected) != 1 || selected[0].TransactionID != high.TransactionID {
+		t.Fatalf("selected=%v want only high fee-rate transaction", selected)
+	}
+	if fees != highFee {
+		t.Fatalf("selected fees=%d want=%d", fees, highFee)
 	}
 }
