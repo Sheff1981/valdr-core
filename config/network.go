463cb470de108d066fdaaeede4935f959655147c
Sheff1981 <linaelizabethvoss@gmail.com>
2026-09-23 20:13:37 +0300

    v0.2: define difficulty retarget window

--- config/network.go
+++ config/network.go
@@ -12,6 +12,7 @@ const (
 	TargetBlockTimeSeconds    = int64(60)
 	PowLimitLeadingZeroBits   = uint(12)
 	DifficultyAdjustmentClamp = uint64(4)
+	DifficultyWindowBlocks     = uint64(10)
 	InitialMiningRewardVDR    = uint64(50)
 	InitialMiningReward       = InitialMiningRewardVDR * AtomicUnitsPerVDR
 	MaxSupplyVDR              = uint64(21_000_000)
