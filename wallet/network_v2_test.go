eac16dd0d529898d5cd9efe2ec24092326ff4cfe
Sheff1981 <linaelizabethvoss@gmail.com>
2026-09-24 08:50:58 +0300

    test: build network-bound wallet transactions

---
+++ wallet/network_v2_test.go
@@ -0,0 +1,52 @@
+package wallet
+
+import (
+	"testing"
+	"time"
+
+	"github.com/Sheff1981/valdr-core/config"
+	"github.com/Sheff1981/valdr-core/core/transaction"
+	"github.com/Sheff1981/valdr-core/core/utxo"
+)
+
+func TestCreateTransactionForChainBuildsV2NetworkBoundTransaction(t *testing.T) {
+	profile, err := config.ResolveNetworkProfile(config.NetworkTestnetV02)
+	if err != nil {
+		t.Fatal(err)
+	}
+	source, err := New("network-source")
+	if err != nil {
+		t.Fatal(err)
+	}
+	recipient, err := New("network-recipient")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx, fee, err := source.CreateTransactionForChain(
+		profile.ChainID,
+		[]utxo.UTXO{{
+			TransactionID: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+			OutputIndex:   0,
+			Amount:        1_000_000,
+			Recipient:     source.Address,
+		}},
+		recipient.Address,
+		100_000,
+		profile.MinRelayFeePerByte,
+		time.Now().UTC().Unix(),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if tx.Version != transaction.VersionV2 ||
+		tx.ChainID != profile.ChainID {
+		t.Fatalf("unexpected transaction network identity: %+v", tx)
+	}
+	if err := tx.ValidateForChain(profile.ChainID); err != nil {
+		t.Fatal(err)
+	}
+	if fee < uint64(tx.SerializedSize())*profile.MinRelayFeePerByte {
+		t.Fatalf("fee=%d size=%d rate=%d", fee, tx.SerializedSize(), profile.MinRelayFeePerByte)
+	}
+}
