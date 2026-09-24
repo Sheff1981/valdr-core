a33b023111586367940bc26a0e197dbc72aa7c23
Sheff1981 <linaelizabethvoss@gmail.com>
2026-09-24 03:03:59 +0300

    v0.2.2 stage 7: test headers-first reorg sync

--- p2p/sync_runtime_v02_test.go
+++ p2p/sync_runtime_v02_test.go
@@ -244,3 +244,131 @@ func waitForSameTip(
 		source.Tip(),
 	)
 }
+
+
+func TestV2HeadersFirstSyncReorgsToGreaterChainwork(t *testing.T) {
+	profile, err := config.ResolveNetworkProfile(config.NetworkDevnetV02)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sourceChain, err := blockchain.NewForProfile(profile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	targetChain, err := blockchain.NewForProfile(profile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	commonKey, err := valdrcrypto.GenerateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	commonMiner, err := valdrcrypto.AddressFromPublicKey(&commonKey.PublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sourceKey, err := valdrcrypto.GenerateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	sourceMiner, err := valdrcrypto.AddressFromPublicKey(&sourceKey.PublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	targetKey, err := valdrcrypto.GenerateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	targetMiner, err := valdrcrypto.AddressFromPublicKey(&targetKey.PublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i := int64(1); i <= 2; i++ {
+		mined, err := mining.MineBlock(
+			sourceChain,
+			commonMiner,
+			profile.GenesisTimestamp+i*profile.TargetBlockTimeSeconds,
+			nil,
+		)
+		if err != nil {
+			t.Fatalf("mine common block %d: %v", i, err)
+		}
+		if err := targetChain.AddBlock(mined); err != nil {
+			t.Fatalf("copy common block %d: %v", i, err)
+		}
+	}
+
+	var oldTargetTip string
+	for i := int64(3); i <= 4; i++ {
+		mined, err := mining.MineBlock(
+			targetChain,
+			targetMiner,
+			profile.GenesisTimestamp+i*profile.TargetBlockTimeSeconds,
+			nil,
+		)
+		if err != nil {
+			t.Fatalf("mine target branch block %d: %v", i, err)
+		}
+		oldTargetTip = mined.BlockHash
+	}
+
+	for i := int64(3); i <= 5; i++ {
+		if _, err := mining.MineBlock(
+			sourceChain,
+			sourceMiner,
+			profile.GenesisTimestamp+i*profile.TargetBlockTimeSeconds,
+			nil,
+		); err != nil {
+			t.Fatalf("mine source branch block %d: %v", i, err)
+		}
+	}
+
+	source := mustStartNode(t, NodeConfig{
+		NodeID:         "v2-reorg-source",
+		ListenAddress:  "127.0.0.1:0",
+		NetworkProfile: &profile,
+		EnableV2:       true,
+		Blockchain:     sourceChain,
+	})
+	defer source.Close()
+
+	target := mustStartNode(t, NodeConfig{
+		NodeID:         "v2-reorg-target",
+		ListenAddress:  "127.0.0.1:0",
+		NetworkProfile: &profile,
+		EnableV2:       true,
+		Blockchain:     targetChain,
+	})
+	defer target.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	if err := target.Connect(ctx, source.Address()); err != nil {
+		t.Fatalf("target connect source: %v", err)
+	}
+	waitForSameTip(t, targetChain, sourceChain, 5*time.Second)
+
+	if _, ok := targetChain.BlockByHash(oldTargetTip); !ok {
+		t.Fatal("disconnected target-branch block was not retained as side branch")
+	}
+	targetBalance, err := targetChain.Balance(targetMiner)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if targetBalance != 0 {
+		t.Fatalf("disconnected branch miner balance=%d want=0", targetBalance)
+	}
+	sourceBalance, err := targetChain.Balance(sourceMiner)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if sourceBalance != 3*profile.InitialSubsidyVDR*config.AtomicUnitsPerVDR {
+		t.Fatalf(
+			"source branch miner balance=%d want=%d",
+			sourceBalance,
+			3*profile.InitialSubsidyVDR*config.AtomicUnitsPerVDR,
+		)
+	}
+}
