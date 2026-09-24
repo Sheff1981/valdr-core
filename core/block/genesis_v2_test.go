8ae0204c16619f77cf281fdc2e02b59093d0260a
Sheff1981 <linaelizabethvoss@gmail.com>
2026-09-24 06:40:11 +0300

    v0.2 stage 11: test frozen Testnet Genesis

--- core/block/genesis_v2_test.go
+++ core/block/genesis_v2_test.go
@@ -1,10 +1,10 @@
 package block
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/Sheff1981/valdr-core/config"
+	"github.com/Sheff1981/valdr-core/core/consensus"
 )
 
 func TestDevnetV02GenesisGolden(t *testing.T) {
@@ -26,12 +26,30 @@ func TestDevnetV02GenesisGolden(t *testing.T) {
 	}
 }
 
-func TestTestnetGenesisRemainsUnfrozen(t *testing.T) {
+func TestTestnetV02GenesisGolden(t *testing.T) {
 	profile, err := config.ResolveNetworkProfile(config.NetworkTestnetV02)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if _, err := NewGenesisForProfile(profile); !errors.Is(err, ErrGenesisNotFrozen) {
-		t.Fatalf("Testnet Genesis error=%v want ErrGenesisNotFrozen", err)
+	genesis, err := NewGenesisForProfile(profile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if genesis.Version != VersionV2 ||
+		genesis.Height != 0 ||
+		genesis.ChainID != "valdr-testnet-1" ||
+		genesis.Timestamp != config.TestnetV02GenesisTimestamp ||
+		genesis.Target != config.TestnetV02GenesisTarget ||
+		genesis.Nonce != config.TestnetV02GenesisNonce ||
+		genesis.ExtraData != config.TestnetV02GenesisMessage ||
+		genesis.BlockHash != config.TestnetV02GenesisHash {
+		t.Fatalf("unexpected Testnet Genesis: %+v", genesis)
+	}
+	target, err := consensus.ParseTargetHexV2(genesis.Target)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := consensus.ValidatePoWTarget(genesis.BlockHash, target); err != nil {
+		t.Fatalf("frozen Testnet Genesis PoW invalid: %v", err)
 	}
 }
